9e295102afaca096ce7bfbdc3b15e5a0365c93f9
Blackstee <ya.nastuha24@gmail.com>
2016-12-27 08:11:56 +0200

    lab3

--- labs/lab3/routes/index.js
+++ labs/lab3/routes/index.js
@@ -1,13 +1,37 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
 
-/* GET home page. */
+var cakes;
+
+fs.readFile('cakes.json', 'utf8', (err, data) => {
+  if (err) throw err;
+  cakes = JSON.parse(data);
+  console.log(cakes);
+});
+
 router.get('/', function(req, res, next) {
   res.render('index');
 });
 
+router.get('/info', function (req, res, next){
+  res.render ('info');
+})
+
+router.get('/cakes/:id', function(req, res, next) {
+  res.render('cake', {
+      id :cakes['cakes'][req.params.id]['id'],
+      name: cakes['cakes'][req.params.id]['name'],
+      color: cakes['cakes'][req.params.id]['color'],
+      rating: cakes['cakes'][req.params.id]['rating'],
+      weight: cakes['cakes'][req.params.id]['weight'],
+       img: cakes['cakes'][req.params.id]['img']
+  });
+});
+
+
 router.get ('/cakes', function (req, res, next) {
-    res.render ('objects', {objects : res.json (object)});
+  res.render('cakes', {cakes: cakes['cakes']} );
 })
 
 module.exports = router;
